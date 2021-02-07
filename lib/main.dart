69f8da9aeadb9b1ab13c976bcaf8b745040f7ba7
Anhar <anharx@gmail.com>
2021-02-07 15:17:51 +0530

    BUG FIX

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pomodoro_timer/pomodoro/PomodoroTimerPage.dart';
-
+import 'pomodoro/pomodoro_timer_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
